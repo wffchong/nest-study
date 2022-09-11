86447c29302428c4873ef216c86e3099e4e324dd
wffchong <283953455@qq.com>
2022-09-11 17:48:46 +0800

    基本类型的提供者注册

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 
+const configService = {
+    provide: 'config',
+    useValue: {
+        name: 'testName',
+        author: 'testAuthor'
+    }
+}
+
 @Module({
     imports: [],
     controllers: [AppController],
-    providers: [AppService]
+    providers: [AppService, configService]
 })
 export class AppModule {}
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,10 @@
-import { Injectable } from '@nestjs/common'
+import { Inject, Injectable } from '@nestjs/common'
 
 @Injectable()
 export class AppService {
-    getHello(): string {
-        return 'Hello World!'
+    constructor(@Inject('config') private config: any) {}
+
+    findOne() {
+        return 'findOne' + this.config.name
     }
 }
