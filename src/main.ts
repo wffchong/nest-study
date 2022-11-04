d1e32bb9e35c33fce379536b6dc2e97fd6554846
wffchong <283953455@qq.com>
2022-11-04 16:17:13 +0800

    使用过滤器处理错误

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
+import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { Validate } from './validate'
+import { ValidateExceptionFilter } from './validate-exception.filter'
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule)
+    // 使用系统管道，Dto实现验证
+    // 全局管道
+    // app.useGlobalPipes(new ValidationPipe())
+    // 使用自定义的
+    app.useGlobalPipes(new Validate())
+    // 使用过滤器
+    app.useGlobalFilters(new ValidateExceptionFilter())
     await app.listen(3000)
 }
 bootstrap()
