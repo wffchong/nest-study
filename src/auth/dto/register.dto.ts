beb9e764811cb591e7fa25040a2b1797c6b29638
wffchong <283953455@qq.com>
2022-11-04 17:41:21 +0800

    自定义校验规则

--- src/auth/dto/register.dto.ts
+++ src/auth/dto/register.dto.ts
@@ -1,9 +1,13 @@
-import { IsNotEmpty } from 'class-validator'
+import { IsNotEmpty, Validate } from 'class-validator'
+import { IsConfirmed } from 'src/rules/is-confirmed.rule'
 
 export default class RegisterDto {
     @IsNotEmpty({ message: '用户名不能为空' })
     name: string
 
+    // 使用装饰器验证
     @IsNotEmpty({ message: '密码不能为空' })
+    // 使用类验证
+    @Validate(IsConfirmed, { message: '密码和确认密码不一致' })
     password: string
 }
