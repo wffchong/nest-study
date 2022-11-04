d619b77ba82142e211677675ef1983157596a558
wffchong <283953455@qq.com>
2022-11-04 18:02:22 +0800

    使用装饰器实现用户注册唯一验证

--- src/auth/dto/register.dto.ts
+++ src/auth/dto/register.dto.ts
@@ -1,8 +1,10 @@
 import { IsNotEmpty, Validate } from 'class-validator'
 import { IsConfirmed } from 'src/rules/is-confirmed.rule'
+import { IsNotExistsRule } from 'src/rules/is-not-exists.rule'
 
 export default class RegisterDto {
     @IsNotEmpty({ message: '用户名不能为空' })
+    @IsNotExistsRule('user', { message: '用户名已经存在' })
     name: string
 
     // 使用装饰器验证
--- src/rules/is-not-exists.rule.ts
+++ src/rules/is-not-exists.rule.ts
@@ -1,1 +1,27 @@
-export const IsNotExistsRule = () => {}
+import { PrismaClient } from '@prisma/client'
+import { registerDecorator, ValidationArguments, ValidationOptions } from 'class-validator'
+
+//表字段是否唯一
+export function IsNotExistsRule(table: string, validationOptions?: ValidationOptions) {
+    return function (object: Record<string, any>, propertyName: string) {
+        registerDecorator({
+            name: 'IsNotExistsRule',
+            target: object.constructor,
+            propertyName: propertyName,
+            constraints: [table],
+            options: validationOptions,
+            validator: {
+                async validate(value: string, args: ValidationArguments) {
+                    const prisma = new PrismaClient()
+                    // console.log(table);
+                    const user = await prisma[table].findFirst({
+                        where: {
+                            [propertyName]: args.value
+                        }
+                    })
+                    return !Boolean(user)
+                }
+            }
+        })
+    }
+}
