0f920ee10345f900b1e6c35a944426b03846b266
wffchong <283953455@qq.com>
2022-11-11 15:20:22 +0800

    完成登录注册

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common'
-import { AuthModule } from './auth/auth.module'
 import { PrismaModule } from './prisma/prisma.module'
+import { AuthModule } from './auth/auth.module'
 @Module({
-    imports: [AuthModule, PrismaModule]
+    imports: [PrismaModule, AuthModule]
 })
 export class AppModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,37 +1,33 @@
 import { BadRequestException, Injectable } from '@nestjs/common'
-import md5 from 'md5'
 import { PrismaService } from 'src/prisma/prisma.service'
-import LoginDto from './dto/login.dto'
-import RegisterDto from './dto/register.dto'
-
+import { RegisterDto } from './dto/register.dto'
+import { hash, verify } from 'argon2'
+import { LoginDto } from './dto/login.dto'
 @Injectable()
 export class AuthService {
     constructor(private readonly prisma: PrismaService) {}
+
     async register(dto: RegisterDto) {
-        const password = md5(dto.password)
-        const user = await this.prisma.user.create({
+        const user = this.prisma.user.create({
             data: {
                 name: dto.name,
-                password
+                password: await hash(dto.password)
             }
         })
-
-        delete user.password
         return user
     }
 
     async login(dto: LoginDto) {
-        const user = await this.prisma.user.findFirst({
+        const user = await this.prisma.user.findUnique({
             where: {
                 name: dto.name
             }
         })
-        //校对密码
-        if (md5(user.password) !== md5(dto.password)) {
+
+        if (!(await verify(user.password, dto.password))) {
             throw new BadRequestException('密码输入错误')
         }
 
-        delete user.password
         return user
     }
 }
