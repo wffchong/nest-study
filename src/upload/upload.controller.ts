3b900da3fdcd7eb91808b235c502cae5c593a8f1
wffchong <283953455@qq.com>
2022-11-12 16:12:27 +0800

    上次图片

--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common'
+import { Controller, MethodNotAllowedException, Post, UploadedFile, UseInterceptors } from '@nestjs/common'
 import { FileInterceptor } from '@nestjs/platform-express'
 import { TransformInterceptor } from 'src/TransformInterceptor'
 
@@ -11,6 +11,30 @@ export class UploadController {
     @UseInterceptors(FileInterceptor('file'))
     uploadFile(@UploadedFile() file: Express.Multer.File) {
         console.log(file)
+        return file
+    }
+
+    @Post('image')
+    // 上传图片
+    @UseInterceptors(
+        FileInterceptor('file', {
+            limits: {
+                fileSize: Math.pow(1024, 2) * 2 // 2 M
+            },
+            fileFilter(
+                req: any,
+                file: Express.Multer.File,
+                callback: (error: Error | null, acceptFile: boolean) => void
+            ): void {
+                if (!file.mimetype.includes('image')) {
+                    callback(new MethodNotAllowedException('文件类型错误'), false)
+                } else {
+                    callback(null, true)
+                }
+            }
+        })
+    )
+    uploadImg(@UploadedFile() file: Express.Multer.File) {
         return file
     }
 }
